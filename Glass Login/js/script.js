2cd99ac4c5f8f638cbe82738a1ff9d25b688fc8c
mirandaph <phmcolombo.ph@gmail.com>
2022-03-22 12:36:27 -0300

    Atualizando projeto

--- Glass Login/js/script.js
+++ Glass Login/js/script.js
@@ -1,13 +1,12 @@
+// Variáveis
 let user = document.getElementById('username')
 let email = document.getElementById('email')
 let pass = document.getElementById('password')
 let btn = document.querySelector('#btn')
+let errorMessage = document.querySelector('.msg')
 
+// Redirecionamento página de sucesso
 function login() {
-    btn.addEventListener("click", function(e){
-        e.preventDefault()
-    })
-
     let userLen = user.value.length
     let emailLen = email.value.length
     let passLen = pass.value.length
@@ -15,8 +14,42 @@ function login() {
     if (userLen >= 1 && emailLen >= 1 && passLen >= 1) {
         window.location.href = "sucess.html"
     }
-
-    console.log(userLen)
-    console.log(emailLen)
-    console.log(passLen)
 }
+
+// Pressionar botão com a key "Enter"
+document.addEventListener("keypress", function(i){
+    if(i.key == "Enter"){
+        btn.click()
+    }
+}) 
+
+// Validação
+btn.addEventListener('click', function(e) {
+    let userVal = user.value
+    let emailVal = email.value
+    let passVal = pass.value
+    // Mensagem de validação
+    if (userVal == '' || emailVal == '' || passVal == '') {
+        errorMessage.textContent = "Please, fill all the fields correctly!"
+        errorMessage.classList = 'error'
+    }
+    // Validação input
+    if (userVal == '') {
+        user.classList = 'errorInput'
+    } else {
+        user.classList.remove = 'errorInput'
+    }
+    
+    if (emailVal == '') {
+        email.classList = 'errorInput'
+    }
+    
+    if (passVal == '') {
+        pass.classList = 'errorInput'
+    }
+
+    // Validação Email
+    if (emailVal.indexOf("@") == -1 || emailVal.indexOf(".") == -1 || emailVal.indexOf(".") == -1 || emailVal.indexOf("@") == -1 ) {
+        email.classList = 'errorInput'
+    }
+})
